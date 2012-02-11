2ac6de2e574407a9e62cee78e02a0c0da171f3d5
Drew Cross <mstngdrew212@gmail.com>
2012-02-11 14:09:15 -0600

    the real fix of settings.py

--- settings.py
+++ settings.py
@@ -13,8 +13,8 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'tatt',                      # Or path to database file if using sqlite3.
-        'USER': 'drew212',                      # Not used with sqlite3.
+        'NAME': '',                      # Or path to database file if using sqlite3.
+        'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
