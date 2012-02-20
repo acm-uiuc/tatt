9a22d2e2502dc1666a4d167c368026312f6329c2
Alex Hendrix <amhendr2@illinois.edu>
2012-02-19 21:18:35 -0600

    Updated models.py to match ER diagram

--- main/models.py
+++ main/models.py
@@ -1,11 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Items(models.Model):
-    itemType = models.ForeignKey('ItemTypes')
+    item_type = models.ForeignKey('ItemTypes')
     name = models.CharField(max_length=100)
+    location = models.CharField(max_length = 100)
+    due_date = models.DateField(auto_now_add = True)
+    owner_id = models.ManyToManyField(User)
+    last_accounted_for = models.ManyToManyField(User)
+    has_photo = models.CharField(max_length = 100)
+    checked_out_by = models.ManyToManyField(User)
 
 class ItemTypes(models.Model):
     name = models.CharField(max_length=100)
 
 class Attributes(models.Model):
     name = models.CharField(max_length=100)
+    value = models.CharField(max_length=100)
+    item_type = models.ForeignKey(ItemTypes)
