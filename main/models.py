53cc52f3927d530081088d7b032981f43f299be5
Drew Cross <mstngdrew212@gmail.com>
2012-05-14 11:48:33 -0500

    Fixed model bug and added stylesheets.

--- main/models.py
+++ main/models.py
@@ -2,14 +2,14 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Items(models.Model):
-    item_type = models.ForeignKey(ItemTypes)
+    item_type = models.ForeignKey('ItemTypes')
     name = models.CharField(max_length=100)
     location = models.CharField(max_length = 100)
     due_date = models.DateField(auto_now_add = True)
-    owner_id = models.ManyToManyField(User)
-    last_accounted_for = models.ManyToManyField(User)
+    owner_id = models.ManyToManyField(User, related_name='owner_id')
+    checked_out_by = models.ManyToManyField(User, related_name='checked_out_by')
+    last_accounted_for = models.DateField()
     has_photo = models.CharField(max_length = 100)
-    checked_out_by = models.ManyToManyField(User)
 
 class ItemTypes(models.Model):
     name = models.CharField(max_length=100)
--- main/views.py
+++ main/views.py
@@ -1,8 +1,10 @@
 from django.template import Context
 from django.shortcuts import render_to_response
+from django.http import HttpResponseRedirect
+from django.template import RequestContext
 
 def index(request):
-    c = Context({
+    c = RequestContext(request, {
             'page_title' : 'index',
         })
     return render_to_response('index.html', c)
