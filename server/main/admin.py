d5b542e9f56b0b56c7e812a03b4fb26f04eef298
Drew Cross <ddcross2@gmail.com>
2012-11-02 21:20:15 -0500

    registration page working

---
+++ server/main/admin.py
@@ -0,0 +1,7 @@
+from models import *
+from django.contrib import admin
+
+admin.site.register(Items)
+admin.site.register(ItemTypes)
+admin.site.register(Attributes)
+admin.site.register(AttributeValues)
