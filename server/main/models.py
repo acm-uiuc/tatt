3095f04f2956b07abe0ddc692519bf75cfe4a044
Drew Cross <mstngdrew212@gmail.com>
2013-03-05 20:14:16 -0600

    updated models and admin interface

--- server/main/models.py
+++ server/main/models.py
@@ -11,14 +11,26 @@ class Item(models.Model):
     last_accounted_for = models.DateField(auto_now_add = True)
     has_photo = models.BooleanField()
 
-class Attribute(models.Model):
-    name = models.CharField(max_length=100)
+    def __unicode__(self):
+        return self.name
 
 class ItemType(models.Model):
     name = models.CharField(max_length=100)
-    attributes = models.ManyToManyField(Attribute, related_name='attribute')
+
+    def __unicode__(self):
+        return self.name
+
+class Attribute(models.Model):
+    name = models.CharField(max_length=100)
+    item_type = models.ForeignKey(ItemType, related_name='item_type')
+    
+    def __unicode__(self):
+        return self.name
 
 class AttributeValue(models.Model):
     item = models.ForeignKey(Item)
     attribute = models.ForeignKey(Attribute)
     value = models.CharField(max_length=100)
+
+    def __unicode__(self):
+        return self.value
