a2c7f4fbd5aee89e9bf8c136b87465b47ad11305
Ian Pickering <ipickering2@gmail.com>
2014-03-25 14:39:54 -0500

    add admin command to check and mail users, overdue form, and email in register form

--- server/main/management/commands/email-user.py
+++ server/main/management/commands/email-user.py
@@ -14,7 +14,7 @@ class Command(BaseCommand):
             if i.due_date != None:
                 if i.due_date <= date.today() and not i.is_overdue:
                     print "Mailing " + i.checked_out_by.username + " at " + i.checked_out_by.email + " about " + i.name;
-                    # i.is_overdue = True;
+                    i.is_overdue = True;
                     send_mail('Track All The Things: Item Overdue', 'The item ' + i.name + ' is overdue. Please return it soon.', 'tatt@acm.illinois.edu', [i.checked_out_by.email], fail_silently=False)
 
 
